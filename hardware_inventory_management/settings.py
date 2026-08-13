c7002ec431cf83ef5222350de0b151f277bd933f
Binayak Prajapati <binayak.praz23@gmail.com>
2026-08-13 12:15:53 +0545

    Fix Docker configuration, add PostgreSQL support, ignore venv and pycache

--- hardware_inventory_management/settings.py
+++ hardware_inventory_management/settings.py
@@ -78,15 +78,33 @@ TEMPLATES = [
 WSGI_APPLICATION = 'hardware_inventory_management.wsgi.application'
 
 
+import os
+
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+POSTGRES_DB = os.environ.get('POSTGRES_DB')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+
+if POSTGRES_DB or POSTGRES_HOST:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': POSTGRES_DB or 'django_db',
+            'USER': os.environ.get('POSTGRES_USER', 'django_user'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'django_pass'),
+            'HOST': POSTGRES_HOST or 'db',
+            'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
