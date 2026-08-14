25ce7445dbc9ef4555012749410369acd54d3e53
Binayak Prajapati <binayak.praz23@gmail.com>
2026-08-14 15:47:38 +0545

    fix: add missing Vendor model definition to inventory/models.py

--- inventory/models.py
+++ inventory/models.py
@@ -183,3 +183,13 @@ class Printer(BaseModel):
 
     def __str__(self):
         return self.ims_code
+
+class Vendor(BaseModel):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    contact_person = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
