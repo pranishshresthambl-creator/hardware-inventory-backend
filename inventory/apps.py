720de6c5c487b9f5a4fb1bcfdbaa7831618f4220
Binayak Prajapati <binayak.praz23@gmail.com>
2026-09-02 12:52:09 +0545

    fix: enable SQLite WAL mode, 60s busy timeout, and safe foreign key cache resolution

--- inventory/apps.py
+++ inventory/apps.py
@@ -1,5 +1,21 @@
 from django.apps import AppConfig
+from django.db.backends.signals import connection_created
+
+
+def set_sqlite_pragmas(sender, connection, **kwargs):
+    if connection.vendor == 'sqlite':
+        try:
+            cursor = connection.cursor()
+            cursor.execute('PRAGMA journal_mode=WAL;')
+            cursor.execute('PRAGMA busy_timeout=60000;')
+            cursor.execute('PRAGMA synchronous=NORMAL;')
+        except Exception:
+            pass
 
 
 class InventoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventory'
+
+    def ready(self):
+        connection_created.connect(set_sqlite_pragmas)
